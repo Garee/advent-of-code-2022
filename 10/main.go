fee1295bc76bbfa19b520e643e904167de9cd0f9
Gary Blackwood <gary@garyblackwood.co.uk>
2022-12-10 09:28:10 +0000

    feat: add solution for day 10 part 2

--- 10/main.go
+++ 10/main.go
@@ -45,29 +45,68 @@ func ParseInstructions(lines []string) []Inst {
 	return instructions
 }
 
-func SimulateSignalStrengths(instructions []Inst) []int {
-	strengths := make([]int, 0)
+func SimulateSignal(instructions []Inst) []int {
+	signal := make([]int, 0)
 
 	x := 1
-	cycle := 1
 	for _, instr := range instructions {
 		for instr.cycle > 0 {
 			instr.cycle--
-			strengths = append(strengths, x*cycle)
-			cycle++
+			signal = append(signal, x)
 		}
 		x += instr.val
+	}
+
+	return signal
+}
+
+func SimulateSignalStrengths(signal []int) []int {
+	strengths := make([]int, 0)
+
+	for cycle, s := range signal {
+		strengths = append(strengths, s*(cycle+1))
 	}
 
 	return strengths
 }
 
+func Draw(signal []int) {
+	width := 40
+	height := 6
+
+	pixels := make([]string, width*height)
+	for i := range pixels {
+		pixels[i] = "."
+	}
+
+	for row := 0; row < height; row++ {
+		for col := 0; col < width; col++ {
+			s := signal[row*width+col]
+			if col == s || col == s-1 || col == s+1 {
+				pixels[row*width+col] = "#"
+			}
+		}
+	}
+
+	for row := 0; row < height; row++ {
+		for col := 0; col < width; col++ {
+			pixel := pixels[row*width+col]
+			fmt.Print(pixel)
+		}
+		fmt.Println()
+	}
+}
+
 func main() {
 	lines := ReadLines()
 	instructions := ParseInstructions(lines)
-	strengths := SimulateSignalStrengths(instructions)
+	signal := SimulateSignal(instructions)
+	strengths := SimulateSignalStrengths(signal)
 
 	// Part 1
 	sum := strengths[19] + strengths[59] + strengths[99] + strengths[139] + strengths[179] + strengths[219]
 	fmt.Println(sum)
+
+	// Part 2
+	Draw(signal)
 }
